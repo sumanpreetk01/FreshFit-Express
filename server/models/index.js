c263fe1d30c9288ced880a82de26f60929d9910d
Rajni bala <balatamna16@gmail.com>
2023-09-21 19:19:27 -0400

    Item ,Order and  models created

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const Category = require('./Category');
+const Item = require('./Item');
+const Order = require('./Order');
+const User = require('./User');
+
+module.exports = { Category, Item, Order,User };
