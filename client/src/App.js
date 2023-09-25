b613158d36a46664c2223a8f128edad4864d3a1a
SwathiVinod19 <swathi.vinod@gmail.com>
2023-09-25 17:34:32 -0400

    tried to add nav bar to appjs

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import ContactUs from './pages/Contact';
-
+// import Nav from './components/Nav';
 
 function App(){
   return (
-
+    <div>
+    {/* <Nav /> */}
     <ContactUs />
+    </div>
   )
 }
 
