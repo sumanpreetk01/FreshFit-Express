78088cbe10bc671a707e1cbf3b6942a86e45ea66
soniasebastian <soniasebastian011@gmail.com>
2023-09-27 21:08:30 -0400

    added productitem

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -11,11 +11,12 @@ const expiration = "1h";
 //function authMiddleware for routes allows token to be sent via req.query or headers
 module.exports = { 
     authMiddleware: function (req, res, next) {
+        console.log( req);
         let token = req.query.token || req.headers.authorization;
 
         //Token bearer & token value
         if (req.headers.authorization) {
-            token = token.split('').pop().trim();
+            token = token.split(' ').pop().trim();
         }
         if(!token) {
             return res.status(400).json({message: "You have no token"});
