02d9a3fcb24940fe4f62b6e23e4cb3d276d6aaf1
SwathiVinod19 <swathi.vinod@gmail.com>
2023-09-27 20:33:07 -0400

    added product details page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
-import ContactUs from './pages/Contact';
-import Nav from './components/nav';
+// import ContactUs from './pages/Contact';
+import Nav from './components/Nav';
 import Home from './pages/Home';
+// import Detail from './pages/Detail';
+import Footer from './components/footer';
 function App(){
   return (
     <div>
     <Nav />
     <Home />
-    <ContactUs />
+    <Footer />
+  
+   
     </div>
   )
 }
---
+++ client/src/pages/Detail.js
@@ -0,0 +1,131 @@
+import React, { useEffect, useState } from 'react';
+import '../index.css'
+import { Link, useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { useDispatch, useSelector } from 'react-redux';
+import { Button } from '@material-ui/core';
+
+import Cart from '../components/Cart';
+import {
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  ADD_TO_CART,
+  UPDATE_PRODUCTS,
+} from '../utils/actions';
+import { QUERY_PRODUCTS } from '../utils/queries';
+import { idbPromise } from '../utils/helpers';
+import spinner from '../assets/spinner.gif';
+
+function Detail() {
+
+//set up Redux functionality 
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+  const { id } = useParams();
+
+// the useState hook is used to create a currentProduct state variable, which will hold the details of the currently selected product.
+
+  const [currentProduct, setCurrentProduct] = useState({});
+
+  const { loading, data } = useQuery(QUERY_PRODUCTS);
+
+  const { products, cart } = state;
+
+  useEffect(() => {
+
+    // already in global store
+
+    if (products.length) {
+      setCurrentProduct(products.find((product) => product._id === id));
+    }
+
+    // retrieved from server
+
+    else if (data) {
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products,
+      });
+
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
+    }
+    
+    // get cache from idb
+    else if (!loading) {
+      idbPromise('products', 'get').then((indexedProducts) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: indexedProducts,
+        });
+      });
+    }
+  }, [products, data, loading, dispatch, id]);
+
+  const addToCart = () => {
+    const itemInCart = cart.find((cartItem) => cartItem._id === id);
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+      });
+      idbPromise('cart', 'put', {
+        ...itemInCart,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...currentProduct, purchaseQuantity: 1 },
+      });
+      idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1 });
+    }
+  };
+
+  const removeFromCart = () => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: currentProduct._id,
+    });
+
+    idbPromise('cart', 'delete', { ...currentProduct });
+  };
+
+  return (
+    <>
+      {currentProduct && cart ? (
+        <div className="container my-1">
+          <Link to="/menu">← Back to Products</Link>
+
+          <h2 className = "header-title">{currentProduct.name}</h2>
+
+          <p>{currentProduct.description}</p>
+
+          <p>
+            <strong>Price:</strong>${currentProduct.price}{' '}
+            <Button color='primary' variant="contained" onClick={addToCart}>Add to Cart</Button>
+            <Button
+              color='primary'
+              variant='contained'
+              disabled={!cart.find((p) => p._id === currentProduct._id)}
+              onClick={removeFromCart}
+            >
+              Remove from Cart
+            </Button>
+          </p>
+
+          <img
+            src={`/images/${currentProduct.image}`}
+            alt={currentProduct.name}
+          />
+        </div>
+      ) : null}
+      {loading ? <img src={spinner} alt="loading" /> : null}
+      <Cart />
+    </>
+  );
+}
+
+export default Detail;
