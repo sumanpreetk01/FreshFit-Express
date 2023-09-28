348e6c8a8427cd952b2aa73339e2f5b8c7a86c4b
Sumanpreet Kaur <sharankaur@sharans-air.lan>
2023-09-27 21:17:00 -0400

    updated app.js and added success page

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,72 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
-
-import Nav from './components/Nav';
 import Home from './pages/Home';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
-function App(){
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() {
   return (
-    <div>
-    <Nav />
-    <Home />
-    <Footer />
-  
-   
-    </div>
-
-  )
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <StoreProvider>
+            <Nav />
+            <Routes>
+              <Route 
+                path="/" 
+                element={<Home />} 
+              />
+              <Route 
+                path="/login" 
+                element={<Login />} 
+              />
+              <Route 
+                path="/signup" 
+                element={<Signup />} 
+              />
+              <Route 
+                path="/success" 
+                element={<Success />} 
+              />
+              <Route 
+                path="/orderHistory" 
+                element={<OrderHistory />} 
+              />
+              <Route 
+                path="/products/:id" 
+                element={<Detail />} 
+              />
+            </Routes>
+            <Footer />
+          </StoreProvider>
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
 }
-
 
 export default App;
