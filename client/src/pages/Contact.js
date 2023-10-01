6b66d190a5e5ed031197c00ef78a74def4d18ddb
SwathiVinod19 <swathi.vinod@gmail.com>
2023-09-30 23:09:57 -0400

    added contact page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ContactForm from "../components/ContactForm";
-
+import './css/contact.css'
 const ContactUs = () => {
   return (
-    <div className="container">
+    <div className="contact-container">
       <ContactForm />
     </div>
   );
