955d90be68f57588cc71612abede7afb953ed52c
soniasebastian <soniasebastian011@gmail.com>
2023-10-02 10:40:44 -0400

    reverted config

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Fresh-Fit-Express',{
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Fresh-Fit-Express');
+// ,{
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
 
-});
+// });
 
 module.exports = mongoose.connection;
