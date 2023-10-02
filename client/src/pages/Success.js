d1442f0e4d4be69339bd177830a36ba25e8ef730
soniasebastian <soniasebastian011@gmail.com>
2023-10-02 10:23:44 -0400

    takes to home page

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -6,27 +6,34 @@ import { idbPromise } from '../utils/helpers';
 function Success() {
   const [addOrder] = useMutation(ADD_ORDER);
 
+  // useEffect(() => {
+  //   async function saveOrder() {
+  //     const cart = await idbPromise('cart', 'get');
+  //     const products = cart.map((item) => item._id);
+
+  //     if (products.length) {
+  //       const { data } = await addOrder({ variables: { items: products } });
+  //       const productData = data.addOrder.products;
+
+  //       productData.forEach((item) => {
+  //         idbPromise('cart', 'delete', item);
+  //       });
+  //     }
+
+  //     setTimeout(() => {
+  //       window.location.assign('/');
+  //     }, 3000);
+  //   }
+
+  //   saveOrder();
+  // }, [addOrder]);
+
   useEffect(() => {
-    async function saveOrder() {
-      const cart = await idbPromise('cart', 'get');
-      const products = cart.map((item) => item._id);
 
-      if (products.length) {
-        const { data } = await addOrder({ variables: { items: products } });
-        const productData = data.addOrder.products;
-
-        productData.forEach((item) => {
-          idbPromise('cart', 'delete', item);
-        });
-      }
-
-      setTimeout(() => {
-        window.location.assign('/');
-      }, 3000);
-    }
-
-    saveOrder();
-  }, [addOrder]);
+    setTimeout(() => {
+            window.location.assign('/');
+          }, 5000);
+  }, []);
 
   return (
     <div>
